16aabbc1718955f5cacc0000c069912f9d723c3f
Serhiy Onyshchenko <ziohimself@gmail.com>
2024-07-08 19:58:05 +0200

    #2 added typescript config, made sure the main.ts is now the entrypoint.

---
+++ src/main/js/main.ts
@@ -0,0 +1,2 @@
+let message: string = 'Hello, from the TypeScript bundle!';
+console.log(message);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
 const path = require('path');
+const { CleanWebpackPlugin }  = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/main/js/main.js',
+    entry: './src/main/js/main.ts',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'target/public/js'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: [/node_modules/],
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
     mode: 'development',
+    devtool:'source-map',
+    resolve: { extensions: ['.ts'] },
 };
